253d0f6f8add42d3074a847bad84e01787b30c76
Vattelakishore242 <vattelakishore@gmail.com>
2025-09-13 19:17:56 +0530

    Update vite.config.js to allow Render host

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,         // allow external access
-    port: 5173,         // default Vite port
-    strictPort: false,  // fallback if port is busy
-    allowedHosts: ['book-finder-n9ch.onrender.com'] // specific host allowed
+    host: true,
+    allowedHosts: 'all'
   }
-});
+})
