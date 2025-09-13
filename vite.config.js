d7aa9991558901da40952f451ad9a22a9f3e40b5
Vattelakishore242 <vattelakishore@gmail.com>
2025-09-13 19:01:41 +0530

    Update vite.config.js with general config for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,         // allow access from all hosts
+    host: true,         // allow dev server to be accessed externally
     port: 5173,         // default Vite port
-    strictPort: false,  // fallback if port is busy
-    allowedHosts: 'all' // allow any host (Render, CodeSandbox, StackBlitz, etc.)
+    strictPort: false   // fallback if port is busy
   }
 });
